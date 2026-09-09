b89fef9415d0f59c7af4ed5dc0b2f88c9320f36c
Jaya Ganesh <jaya07ganesh@gmail.com>
2026-09-10 01:43:03 +0530

    up

--- JS/Day2/forLooping.js
+++ JS/Day2/forLooping.js
@@ -1,7 +1,22 @@
 //TASK 1 – Print Numbers
 
 
-for (let i = 1; i <= 10; i++) {
-    console.log(i);
+for (let l = 1; l <= 10; l++) {
+    console.log(l);
 }
 
+//TASK 2 – Even Numbers
+
+for (let o = 1; o <= 20; o++) {
+    if (o % 2 === 0) {
+        console.log(o);
+    }
+}
+
+//TASK 3 – Multiplication Table
+
+let num = 5;
+
+for (let i = 1; i <= 10; i++) {
+    console.log(num + " x " + i + " = " + (num * i));
+}
