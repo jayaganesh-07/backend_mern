8e2a38bc64e8dedc55dedbf565f6d22f62eca926
Jaya Ganesh <jaya07ganesh@gmail.com>
2026-09-11 00:18:57 +0530

    updated

--- JS/Day3/callBack.js
+++ JS/Day3/callBack.js
@@ -15,3 +15,28 @@ const send=(result)=>{
 }
 evenNumber(10,send)
 
+
+
+//Task 1
+
+function createBankAccount() {
+
+    let balance = 5000
+
+    function checkBalance() {
+        console.log("Account Balance:", balance)
+    }
+
+    return checkBalance
+}
+
+// Outside balance
+let balance = 100000
+
+console.log("Outside Balance:", balance)
+
+// Create bank account
+let account = createBankAccount()
+
+// Check private account balance
+account()
