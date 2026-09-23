428aefd8b1294f1514f6c8155170c2cb13169100
Jaya Ganesh <jaya07ganesh@gmail.com>
2026-09-23 15:54:18 +0530

    up

---
+++ REACT JS/Day3/navbar/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import NavBar from './Components/NavBar'
+import About from './Components/About'
+import Home from './Components/Home'
+import Contact from './Components/Contact'
+
+const App = () => {
+  return (
+    <>
+    <h1>Welcome To My Page</h1>
+    <NavBar/>
+    <About/>
+    <Home/>
+    <Contact/>
+    
+    </>
+  )
+}
+
+export default App
---
+++ REACT JS/Day3/navbar/src/Components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+<>
+<h1>
+    This Home Page
+</h1>
+
+</>
+  )
+}
+
+export default Home
