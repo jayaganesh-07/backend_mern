23c783c4a2f03854b6e135db6056de5428aa51cf
Jaya Ganesh <jaya07ganesh@gmail.com>
2026-09-25 16:47:02 +0530

    up

---
+++ REACT JS/day5/rendering/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+  <>
+  <nonprimitive/>
+  </>
+  )
+}
+
+export default App
---
+++ REACT JS/day5/rendering/src/assets/component/nonprimitive.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+ 
+const nonprimitive = () => {
+    const array = ["samatha","nayanthara","trisha","pooja hedge","kayadu"]
+
+
+
+  return (
+    <div>{array}</div>
+  )
+}
+
+export default nonprimitive;
